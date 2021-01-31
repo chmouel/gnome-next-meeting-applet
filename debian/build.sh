742dbe48795c0151411db69065fdd773762100e1
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-31 16:32:37 +0100

    Build to rolling and lts
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- debian/build.sh
+++ debian/build.sh
@@ -7,6 +7,9 @@ PPATARGET=chmouel/${NAME}
 GPG_KEY=${GPG_KEY}
 YUBIKEY=${YUBIKEY:-}
 DISTRO_VERSION="groovy"
+RELEASE=4
+VERSION=$(python3 -c 'import gnome_next_meeting_applet as f;print(f.__version__)')
+
 
 [[ -n ${GPG_KEY} ]] || {
     echo "You need to setup your GPG_KEY"
@@ -14,24 +17,27 @@ DISTRO_VERSION="groovy"
 }
 gpg --list-secret-keys >/dev/null || { echo "You need to have a secret GPG key"; exit 1 ;}
 
-sudo docker build -f debian/Dockerfile -t ${NAME}-builder .
-
-version=$(python3 -c 'import gnome_next_meeting_applet as f;print(f.__version__)')
-release=1
-
-fpath=$(readlink -f .)
-sudo docker run --rm \
-       -v ~/.gnupg:/home/builder/.gnupg \
-       -v ${fpath}:/home/builder/build/${NAME}-${version}-${release} \
-	    --name ${NAME}-builder \
-       --env USER=${USER} \
-       --env PPATARGET=${PPATARGET} \
-       --env GPG_KEY=${GPG_KEY} \
-       --user root \
-       -it ${NAME}-builder #\
-       # /bin/bash -c "gpg-agent --pinentry-program /usr/bin/pinentry --daemon && \
-       #               cd ${NAME}-${version}-${release} && \
-       #               dch -M -v ${version}-${release} -D ${DISTRO_VERSION} \"new update\" && \
-       #               debuild -S --force-sign -k${GPG_KEY} && \
-       #               cd .. && \
-       #               echo dput ppa:${PPATARGET} *source.changes"
+for dockerdistroversion in latest rolling;do
+    dockerfile=/tmp/Dockerfile.${dockerdistroversion}.$$
+    sed "s/FROM ubuntu:.*/FROM ubuntu:${dockerdistroversion}/" ./debian/Dockerfile > ${dockerfile}
+    sudo docker build -f ${dockerfile} -t ${NAME}-${dockerdistroversion}-builder .
+    fpath=$(readlink -f .)
+    sudo docker run --rm \
+           -v ~/.gnupg:/home/builder/.gnupg \
+           -v ${fpath}:/home/builder/build/src \
+            --name ${NAME}-${dockerdistroversion}-builder \
+           --env USER=${USER} \
+           --env PPATARGET=${PPATARGET} \
+           --env GPG_KEY=${GPG_KEY} \
+           -it ${NAME}-${dockerdistroversion}-builder \
+           /bin/bash -c "gpg-agent --pinentry-program /usr/bin/pinentry --daemon && \
+                         cp -a src/ ${NAME}-${VERSION} && rm -rf ${NAME}-${VERSION}/.git && \
+                         cd ${NAME}-${VERSION} && \
+                         dch -M -v ${VERSION}-${RELEASE}~\`lsb_release -cs\`1 -D \`lsb_release -cs\` \"new update\" && \
+                         dpkg-source -b . && \
+                         debuild -S --force-sign -k${GPG_KEY} && \
+                         cd .. && \
+                         dput ppa:${PPATARGET} *source.changes"
+            rm -f ${dockerfile}
+done
+                         
