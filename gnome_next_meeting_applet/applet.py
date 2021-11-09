fe589c319c455ebe93ddad38882cedd82bd9bccb
Chmouel Boudjnah <chmouel@chmouel.com>
2021-11-09 14:36:08 +0100

    Compare all to UTC
    We need all to be UTC or we will end up mad.
    
    Tested with a few crazy use cases and it all worked...
    
    reformat with black
    
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- gnome_next_meeting_applet/applet.py
+++ gnome_next_meeting_applet/applet.py
@@ -20,81 +20,84 @@ import pathlib
 import re
 import typing
 
-import dateutil.parser as dtparse
 import dateutil.relativedelta as dtrelative
 import dateutil.tz as dttz
-import tzlocal
+import pytz
 import yaml
-
-import gnome_next_meeting_applet.evolution_calendars as evocal
-
 
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Gdk as gdk
 from gi.repository import GLib as glib
 from gi.repository import Gtk as gtk
 
+import gnome_next_meeting_applet.evolution_calendars as evocal
+
 APP_INDICTOR_ID = "gnome-next-meeting-applet"
 
 DEFAULT_CONFIG = {
     # TODO(chmouel): should be plural
-    'restrict_to_calendar': [],
-    'skip_non_accepted': True,
-    'my_emails': [],
-    'max_results': 10,
-    'title_max_char': 20,
-    'refresh_interval': 300,
-    'event_organizers_icon': {},
-    'change_icon_minutes': 2,
-    'default_icon': "‣"
+    "restrict_to_calendar": [],
+    "skip_non_accepted": True,
+    "my_emails": [],
+    "max_results": 10,
+    "title_max_char": 20,
+    "refresh_interval": 300,
+    "event_organizers_icon": {},
+    "change_icon_minutes": 2,
+    "default_icon": "‣",
 }
 
 VIDEOCALL_DESC_REGEXP = [
     r"Join Zoom Meeting\n(https://zoom.us/j/[^\n]*)"
-    r"This event has a video call.\nJoin: (https://meet.google.com/[^\n]*)"]
+    r"This event has a video call.\nJoin: (https://meet.google.com/[^\n]*)"
+]
+
 
 class Applet:
     """Applet: class"""
+
     events: typing.List = []
     indicator = None
 
     def __init__(self):
         self.config = DEFAULT_CONFIG
         self.config_dir = os.path.expanduser(
-            f"{glib.get_user_config_dir()}/{APP_INDICTOR_ID}")
+            f"{glib.get_user_config_dir()}/{APP_INDICTOR_ID}"
+        )
 
         configfile = pathlib.Path(self.config_dir) / "config.yaml"
         if configfile.exists():
-            self.config = {
-                **DEFAULT_CONFIG,
-                **yaml.safe_load(configfile.open())
-            }
+            self.config = {**DEFAULT_CONFIG, **yaml.safe_load(configfile.open())}
         self.autostart_file = pathlib.Path(
             f"{glib.get_user_config_dir()}/autostart/gnome-next-meeting-applet.desktop"
         ).expanduser()
 
     def htmlspecialchars(self, text):
         """Replace html chars"""
-        return (text.replace("&", "&amp;").replace('"', "&quot;").replace(
-            "<", "&lt;").replace(">", "&gt;"))
+        return (
+            text.replace("&", "&amp;")
+            .replace('"', "&quot;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+        )
 
     def first_event(self, event):
         """Show first event in menubar"""
         summary = self.htmlspecialchars(
-            event.get_summary().get_value().strip()[:self.config['title_max_char']])
-        now = datetime.datetime.now(dttz.tzlocal()).astimezone(
-            tzlocal.get_localzone())
+            event.get_summary().get_value().strip()[: self.config["title_max_char"]]
+        )
+        now = datetime.datetime.now().astimezone(pytz.timezone("UTC"))
 
-        start_time = evocal.get_ecal_as_tstzed(event.get_dtstart())
-        end_time = evocal.get_ecal_as_tstzed(event.get_dtend())
+        start_time = evocal.get_ecal_as_utc(event.get_dtstart())
+        end_time = evocal.get_ecal_as_utc(event.get_dtend())
 
         if start_time < now < end_time:
             _rd = dtrelative.relativedelta(end_time, now)
         else:
             _rd = dtrelative.relativedelta(start_time, now)
+
         humzrd = ""
-        for dttype in (("day", _rd.days), ("hour", _rd.hours), ("minute",
-                                                                _rd.minutes)):
+        for dttype in (("day", _rd.days), ("hour", _rd.hours), ("minute", _rd.minutes)):
             if dttype[1] == 0:
                 continue
             humzrd += f"{dttype[1]} {dttype[0]}"
@@ -111,38 +114,51 @@ class Applet:
 
         evolutionCalendar = evocal.EvolutionCalendarWrapper()
         # TODO: add filtering user option GUI instead of just yaml
-        event_list = evolutionCalendar.get_all_events(restrict_to_calendar=self.config["restrict_to_calendar"])
+        event_list = evolutionCalendar.get_all_events(
+            restrict_to_calendar=self.config["restrict_to_calendar"]
+        )
         ret = []
-        for event in sorted(event_list, key=lambda x: evocal.get_ecal_as_tstzed(x.get_dtstart()).timestamp()):
+
+        events_sorted = sorted(
+            event_list, key=lambda x: evocal.get_ecal_as_utc(x.get_dtstart())
+        )
+        for event in events_sorted:
             if event.get_status().value_name != "I_CAL_STATUS_CONFIRMED":
+                print("SKipping not confirmed")
                 continue
 
             skipit = False
-            if self.config['skip_non_accepted']:
+            if self.config["skip_non_accepted"]:
                 skipit = True
                 for attendee in event.get_attendees():
-                    for myemail in self.config['my_emails']:
-                        if attendee.get_value().replace("mailto:", "") == myemail and \
-                           attendee.get_partstat().value_name == 'I_CAL_PARTSTAT_ACCEPTED':
+                    for myemail in self.config["my_emails"]:
+                        if (
+                            attendee.get_value().replace("mailto:", "") == myemail
+                            and attendee.get_partstat().value_name
+                            == "I_CAL_PARTSTAT_ACCEPTED"
+                        ):
                             skipit = False
-
-            
             if skipit:
                 continue
 
             ret.append(event)
+
         return ret
 
     # pylint: disable=unused-argument
     def set_indicator_icon_label(self, source):
-        now = datetime.datetime.now(dttz.tzlocal()).astimezone(
-            tzlocal.get_localzone())
-        first_start_time = evocal.get_ecal_as_tstzed(self.events[0].get_dtstart())
-        first_end_time = evocal.get_ecal_as_tstzed(self.events[0].get_dtend())
+        now = datetime.datetime.now().astimezone(pytz.timezone("UTC"))
+        first_start_time = evocal.get_ecal_as_utc(self.events[0].get_dtstart())
+        first_end_time = evocal.get_ecal_as_utc(self.events[0].get_dtend())
 
-        if now > (first_start_time - datetime.timedelta(
-                minutes=self.config['change_icon_minutes'])
-        ) and not now > first_start_time:
+        if (
+            now
+            > (
+                first_start_time
+                - datetime.timedelta(minutes=self.config["change_icon_minutes"])
+            )
+            and not now > first_start_time
+        ):
             source.set_icon(self.get_icon_path("notification"))
         elif now >= first_end_time:  # need a refresh
             self.make_menu_items()
@@ -150,8 +166,7 @@ class Applet:
         else:
             source.set_icon(self.get_icon_path("calendar"))
 
-        source.set_label(f"{self.first_event(self.events[0])}",
-                         APP_INDICTOR_ID)
+        source.set_label(f"{self.first_event(self.events[0])}", APP_INDICTOR_ID)
         return True
 
     def get_icon_path(self, icon):
@@ -159,7 +174,7 @@ class Applet:
         if not devpath.exists():
             devpath = pathlib.Path("/usr/share/gnome-next-meeting/images")
 
-        for ext in ['svg', 'png']:
+        for ext in ["svg", "png"]:
             if (devpath / f"{icon}.{ext}").exists():
                 return str(devpath / f"{icon}.{ext}")
         return "x-office-calendar-symbolic"
@@ -173,61 +188,64 @@ class Applet:
 
     def make_menu_items(self):
         self.events = self.get_all_events()
-        
-        menu = gtk.Menu()
-        now = datetime.datetime.now(dttz.tzlocal()).astimezone(
-            tzlocal.get_localzone())
 
+        menu = gtk.Menu()
+        now = datetime.datetime.now().astimezone(pytz.timezone("UTC"))
         currentday = ""
 
         event_first = self.events[0]
-        event_first_start_time = evocal.get_ecal_as_tstzed(event_first.get_dtstart())
-        event_first_end_time = evocal.get_ecal_as_tstzed(event_first.get_dtend())
-        
-        if event_first_start_time < now and now < event_first_end_time and event_first.get_attachments():
+        event_first_start_time = evocal.get_ecal_as_utc(event_first.get_dtstart())
+        event_first_end_time = evocal.get_ecal_as_utc(event_first.get_dtend())
+
+        if (
+            event_first_start_time < now
+            and now < event_first_end_time
+            and event_first.get_attachments()
+        ):
             menuitem = gtk.MenuItem(label="📑 Open current meeting document")
             menuitem.location = event_first.get_attachments()[0].get_url()
-            menuitem.connect('activate', self.applet_click)
+            menuitem.connect("activate", self.applet_click)
             menu.add(menuitem)
             menu.append(gtk.SeparatorMenuItem())
             menu.append(gtk.MenuItem(" "))
 
-        for event in self.events[0:int(self.config["max_results"])]:
+        for event in self.events[0 : int(self.config["max_results"])]:
             # TODO print the day
-            start_time = evocal.get_ecal_as_tstzed(event.get_dtstart())
-            _cday = start_time.strftime('%A %d %B %Y')
-            
+            start_time = evocal.get_ecal_as_utc(event.get_dtstart())
+            start_time = start_time.astimezone(dttz.gettz())
+
+            _cday = start_time.strftime("%A %d %B %Y")
+
             if _cday != currentday:
                 if currentday != "":
                     menu.append(gtk.MenuItem(label=""))
                 todayitem = gtk.MenuItem(
-                    label=f'<span size="large" font="FreeSerif:18">{_cday}</span>')
+                    label=f'<span size="large" font="FreeSerif:18">{_cday}</span>'
+                )
                 todayitem.get_child().set_use_markup(True)
                 todayitem.location = f"https://calendar.google.com/calendar/r/day/{start_time.strftime('%Y/%m/%d')}"
-                todayitem.connect('activate', self.applet_click)
+                todayitem.connect("activate", self.applet_click)
                 menu.append(todayitem)
                 menu.append(gtk.SeparatorMenuItem())
                 currentday = _cday
 
             summary = self.htmlspecialchars(event.get_summary().get_value().strip())
-
             organizer = event.get_organizer().get_value().replace("mailto:", "")
 
-            icon = self.config['default_icon']
-            for regexp in self.config['event_organizers_icon']:
+            icon = self.config["default_icon"]
+            for regexp in self.config["event_organizers_icon"]:
                 if re.match(regexp, organizer):
-                    icon = self.config['event_organizers_icon'][regexp]
+                    icon = self.config["event_organizers_icon"][regexp]
                     break
 
             start_time_str = start_time.strftime("%H:%M")
             if now >= start_time:
                 summary = f"<i>{summary}</i>"
-            menuitem = gtk.MenuItem(
-                label=f"{icon} {summary} - {start_time_str}")
+            menuitem = gtk.MenuItem(label=f"{icon} {summary} - {start_time_str}")
             menuitem.get_child().set_use_markup(True)
 
             match_videocall_summary = self._match_videocall_url_from_summary(event)
-            
+
             if event.get_location() and event.get_location().startswith("https://"):
                 menuitem.location = event.get_location()
             elif match_videocall_summary:
@@ -235,26 +253,27 @@ class Applet:
             else:
                 menuitem.location = ""
 
-            menuitem.connect('activate', self.applet_click)
+            menuitem.connect("activate", self.applet_click)
             menu.append(menuitem)
 
         menu.append(gtk.SeparatorMenuItem())
 
         settingMenu = gtk.Menu()
-        label = "Remove autostart" if self.autostart_file.exists(
-        ) else "Auto start at boot"
+        label = (
+            "Remove autostart" if self.autostart_file.exists() else "Auto start at boot"
+        )
         item_autostart = gtk.MenuItem(label=label)
-        item_autostart.connect('activate', self.install_uninstall_autostart)
+        item_autostart.connect("activate", self.install_uninstall_autostart)
         settingMenu.add(item_autostart)
         settingItem = gtk.MenuItem("Setting")
         settingItem.set_submenu(settingMenu)
         menu.add(settingItem)
 
         item_refreh = gtk.MenuItem(label="Refresh")
-        item_refreh.connect('activate', self.make_menu_items)
+        item_refreh.connect("activate", self.make_menu_items)
         menu.add(item_refreh)
         item_quit = gtk.MenuItem(label="Quit")
-        item_quit.connect('activate', self.applet_quit)
+        item_quit.connect("activate", self.applet_quit)
         menu.add(item_quit)
 
         menu.show_all()
@@ -270,7 +289,7 @@ class Applet:
                 url = match.groups()[0]
                 return url
         return ""
-        
+
     def install_uninstall_autostart(self, source):
 
         if self.autostart_file.exists():
@@ -278,7 +297,8 @@ class Applet:
             source.set_label("Auto start at boot")
             return
 
-        self.autostart_file.write_text("""#!/usr/bin/env xdg-open
+        self.autostart_file.write_text(
+            """#!/usr/bin/env xdg-open
 [Desktop Entry]
 Categories=Productivity;
 Comment=Google calendar applet to show next meetings
@@ -288,20 +308,21 @@ Name=Google Calendar next meeting
 StartupNotify=false
 Type=Application
 Version=1.0
-""")
+"""
+        )
         source.set_label("Remove autostart")
 
     def build_indicator(self):
         self.indicator = appindicator.Indicator.new(
-            APP_INDICTOR_ID, self.get_icon_path("calendar"),
-            appindicator.IndicatorCategory.SYSTEM_SERVICES)
+            APP_INDICTOR_ID,
+            self.get_icon_path("calendar"),
+            appindicator.IndicatorCategory.SYSTEM_SERVICES,
+        )
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
         self.make_menu_items()
         self.set_indicator_icon_label(self.indicator)
-        glib.timeout_add_seconds(30, self.set_indicator_icon_label,
-                                 self.indicator)
-        glib.timeout_add_seconds(self.config['refresh_interval'],
-                                 self.make_menu_items)
+        glib.timeout_add_seconds(30, self.set_indicator_icon_label, self.indicator)
+        glib.timeout_add_seconds(self.config["refresh_interval"], self.make_menu_items)
         gtk.main()
 
     def main(self):
@@ -313,5 +334,5 @@ def run():
     c.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
--- gnome_next_meeting_applet/evolution_calendars.py
+++ gnome_next_meeting_applet/evolution_calendars.py
@@ -15,20 +15,17 @@
 # pylint: disable=no-self-use
 #
 # from Gab Leroux (@GabLeRoux) https://askubuntu.com/a/1371087
-import tzlocal
+import pytz
 import datetime
 
-import pytz
 import gi
 
 import dateutil.parser as dtparse
-import dateutil.tz as dttz
 
-
-gi.require_version('EDataServer', '1.2')
+gi.require_version("EDataServer", "1.2")
 from gi.repository import EDataServer
 
-gi.require_version('ECal', '2.0')
+gi.require_version("ECal", "2.0")
 from gi.repository import ECal
 
 from gi.repository import Gio
@@ -36,9 +33,12 @@ from gi.repository import Gio
 # https://lazka.github.io/pgi-docs/Gio-2.0/classes/Cancellable.html#Gio.Cancellable
 GIO_CANCELLABLE = Gio.Cancellable.new()
 
-def get_ecal_as_tstzed(ecalcomp) -> datetime.datetime:
-    tz = ecalcomp.get_tzid() or "UTC"
-    return dtparse.parse(ecalcomp.get_value().as_ical_string()).astimezone(pytz.timezone(tz))
+
+def get_ecal_as_utc(ecalcomp) -> datetime.datetime:
+    tz = pytz.timezone(ecalcomp.get_tzid() or "UTC")
+    ts = ecalcomp.get_value().as_timet()
+    dt = tz.normalize(tz.localize(datetime.datetime.utcfromtimestamp(ts)))
+    return dt.astimezone(pytz.timezone("UTC"))
 
 
 class EvolutionCalendarWrapper:
@@ -46,10 +46,12 @@ class EvolutionCalendarWrapper:
     def _get_gnome_calendars():
         # https://lazka.github.io/pgi-docs/EDataServer-1.2/classes/SourceRegistry.html#EDataServer.SourceRegistry.new_sync
         registry = EDataServer.SourceRegistry.new_sync(GIO_CANCELLABLE)
-        return EDataServer.SourceRegistry.list_sources(registry, EDataServer.SOURCE_EXTENSION_CALENDAR)
+        return EDataServer.SourceRegistry.list_sources(
+            registry, EDataServer.SOURCE_EXTENSION_CALENDAR
+        )
 
     def _get_gnome_events_from_calendar_source(self, source: EDataServer.Source):
-                # https://lazka.github.io/pgi-docs/ECal-2.0/classes/Client.html#ECal.Client
+        # https://lazka.github.io/pgi-docs/ECal-2.0/classes/Client.html#ECal.Client
         client = ECal.Client()
 
         # https://lazka.github.io/pgi-docs/ECal-2.0/classes/Client.html#ECal.Client.connect_sync
@@ -65,24 +67,26 @@ class EvolutionCalendarWrapper:
         if not new_client:
             return events
         # https://lazka.github.io/pgi-docs/ECal-2.0/classes/Client.html#ECal.Client.get_object_list_as_comps_sync
-        ret, values = new_client.get_object_list_as_comps_sync(sexp="#t", cancellable=GIO_CANCELLABLE)
+        ret, values = new_client.get_object_list_as_comps_sync(
+            sexp="#t", cancellable=GIO_CANCELLABLE
+        )
         if not ret:
             return events
 
         for value in values:
             if not value:
                 continue
-            start_time = get_ecal_as_tstzed(value.get_dtstart())
-            end_time = get_ecal_as_tstzed(value.get_dtend())
-            now = datetime.datetime.now(dttz.tzlocal()).astimezone(tzlocal.get_localzone())
+            start_time = get_ecal_as_utc(value.get_dtstart())
+            end_time = get_ecal_as_utc(value.get_dtend())
+            now = datetime.datetime.now().astimezone(pytz.timezone("UTC"))
 
-            if now >= (end_time):
+            if now >= end_time:
                 continue
 
             ## save memory for stuff we won't care
             if start_time > (now + datetime.timedelta(weeks=4)):
                 continue
-                        
+
             uuid = value.get_id().get_uid()
             # sometime we get doublons whatever the reasons is
             if uuid in seen:
