22eaa060f2b2f303e40c68ed46110cd037515e23
Chmouel Boudjnah <chmouel@chmouel.com>
2021-11-11 18:52:37 +0100

    another fix to the make-release script
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- packaging/make-release.sh
+++ packaging/make-release.sh
@@ -8,7 +8,7 @@ vfile=gnome_next_meeting_applet/__init__.py
 sed -i "s/.*version.*/__version__ = '${VERSION}'/" ${vfile}
 git commit -m "Release ${VERSION} 🥳" ${vfile}
 git tag ${VERSION}
-git push origin
+git push --tags origin ${VERSION}
 
 ./debian/build.sh
 ./packaging/rpm/build.sh
