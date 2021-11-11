b5103c7fcb8a95076365e4e966da2c8e517a8d65
Chmouel Boudjnah <chmouel@chmouel.com>
2021-11-11 18:30:28 +0100

    fix release script again
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- packaging/make-release.sh
+++ packaging/make-release.sh
@@ -5,7 +5,7 @@ VERSION=${1-""}
 [[ -z ${VERSION} ]] && { echo "need a version"; exit 1 ;}
 
 vfile=gnome_next_meeting_applet/__init__.py
-sed -i "s/(.*version.*/__version__ = '${VERSION}'/" ${vfile}
+sed -i "s/.*version.*/__version__ = '${VERSION}'/" ${vfile}
 git commit -m "Release ${VERSION} 🥳" ${vfile}
 git tag ${VERSION}
 git push origin
