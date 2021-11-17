e68f80bd299beabc6a3d39f9b495ba7bdb87f122
Chmouel Boudjnah <chmouel@chmouel.com>
2021-11-17 11:47:00 +0100

    don't exit when there is nothing to commit
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- packaging/make-release.sh
+++ packaging/make-release.sh
@@ -6,7 +6,7 @@ VERSION=${1-""}
 
 vfile=gnome_next_meeting_applet/__init__.py
 sed -i "s/.*version.*/__version__ = '${VERSION}'/" ${vfile}
-git commit -S -m "Release ${VERSION} 🥳" ${vfile}
+git commit -S -m "Release ${VERSION} 🥳" ${vfile} || true
 git tag -s ${VERSION}
 git push --tags origin ${VERSION}
 
