fc9801d3bd4c74822ed73123920df27ac51ee100
Chmouel Boudjnah <chmouel@chmouel.com>
2021-11-24 18:44:51 +0100

    Improve release script
    Add bumpversion
    make a proper release
    
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- packaging/make-release.sh
+++ packaging/make-release.sh
@@ -1,14 +1,46 @@
 #!/usr/bin/env bash
-set -eufx
-
+set -euf
 VERSION=${1-""}
-[[ -z ${VERSION} ]] && { echo "need a version"; exit 1 ;}
+
+bumpversion() {
+   current=$(git describe --tags $(git rev-list --tags --max-count=1))
+   echo "Current version is ${current}"
+
+   major=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_major()))" ${current})
+   minor=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_minor()))" ${current})
+   patch=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_patch()))" ${current})
+
+   echo "If we bump we get, Major: ${major} Minor: ${minor} Patch: ${patch}"
+   read -p "To which version you would like to bump [M]ajor, Mi[n]or, [P]atch or Manua[l]: " ANSWER
+   if [[ ${ANSWER,,} == "m" ]];then
+       mode=major
+   elif [[ ${ANSWER,,} == "n" ]];then
+       mode=minor
+   elif [[ ${ANSWER,,} == "p" ]];then
+       mode=patch
+   elif [[ ${ANSWER,,} == "l" ]];then
+       read -p "Enter version: " -e VERSION
+       return
+   else
+       print "no or bad reply??"
+       exit
+   fi
+   VERSION=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_${mode}()))" ${current})
+   [[ -z ${VERSION} ]] && {
+       echo "could not bump version automatically"
+       exit
+   }
+   echo "Releasing ${VERSION}"
+}
+
+[[ -z ${VERSION} ]] && bumpversion
 
 vfile=gnome_next_meeting_applet/__init__.py
 sed -i "s/.*version.*/__version__ = '${VERSION}'/" ${vfile}
 git commit -S -m "Release ${VERSION} 🥳" ${vfile} || true
 git tag -s ${VERSION} -m "Releasing version ${VERSION}"
 git push --tags origin ${VERSION}
+gh release create ${VERSION} --notes "Release ${VERSION} 🥳"
 
 ./debian/build.sh
 ./packaging/aur/build.sh
