5b81dbd3a15624e54606b3c71da4f2c1da0adbb2
Chmouel Boudjnah <chmouel@chmouel.com>
2021-11-30 09:25:07 +0100

    Add debian packaging via poetry
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- debian/build.sh
+++ debian/build.sh
@@ -2,12 +2,27 @@
 set -eux
 
 cd $(git rev-parse --show-toplevel)
-NAME=gnome-next-meeting-applet
+POETRY_NAME_VERSION="$(poetry version)"
+NAME=${POETRY_NAME_VERSION% *}
+VERSION=${POETRY_NAME_VERSION#* }
 PPATARGET=chmouel/${NAME}
-
 RELEASE=1
-VERSION=$(python3 -c 'import gnome_next_meeting_applet as f;print(f.__version__)')
 GPG_KEY=${GPG_KEY}
+
+finalaction="debuild -S --force-sign -k${GPG_KEY} && cd .. && dput ppa:${PPATARGET} *source.changes"
+
+while getopts "d" o; do
+    case "${o}" in
+        d)
+            finalaction="/bin/bash"
+            ;;
+        *)
+            echo "Invalid option"; exit 1;
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 
 [[ -n ${GPG_KEY} ]] || {
     echo "You need to setup your GPG_KEY"
@@ -15,7 +30,7 @@ GPG_KEY=${GPG_KEY}
 }
 gpg --list-secret-keys >/dev/null || { echo "You need to have a secret GPG key"; exit 1 ;}
 
-for dockerdistroversion in latest rolling;do
+for dockerdistroversion in latest ;do
     dockerfile=/tmp/Dockerfile.${dockerdistroversion}.$$
     sed "s/FROM ubuntu:.*/FROM ubuntu:${dockerdistroversion}/" ./debian/Dockerfile > ${dockerfile}
     sudo docker build -f ${dockerfile} -t ${NAME}-${dockerdistroversion}-builder .
@@ -28,14 +43,13 @@ for dockerdistroversion in latest rolling;do
            --env PPATARGET=${PPATARGET} \
            --env GPG_KEY=${GPG_KEY} \
            -it ${NAME}-${dockerdistroversion}-builder \
-           /bin/bash -c "gpg-agent --pinentry-program /usr/bin/pinentry --daemon && \
+           /bin/bash -xc "gpg-agent --pinentry-program /usr/bin/pinentry --daemon && \
                          cp -a src/ ${NAME}-${VERSION} && rm -rf ${NAME}-${VERSION}/.git && \
                          cd ${NAME}-${VERSION} && \
                          dch -M -v ${VERSION}-${RELEASE}~\`lsb_release -cs\`1 -D \`lsb_release -cs\` \"new update\" && \
+                         /home/builder/.local/bin/dephell deps convert --from=pyproject.toml --to=setup.py && \
                          dpkg-source -b . && \
-                         debuild -S --force-sign -k${GPG_KEY} && \
-                         cd .. && \
-                         dput ppa:${PPATARGET} *source.changes"
+                         ${finalaction}"
             rm -f ${dockerfile}
 done
                          
