49a6ad25bdd92cae52f9bda897508bb778d4b989
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-25 13:17:51 +0100

    add some tests

---
+++ tests/test_strings.py
@@ -0,0 +1,37 @@
+import datetime
+
+from gnma import strings
+
+
+# test strings.humanize_time output with pytest
+def test_humanize_already_started():
+    start_time = datetime.datetime.now()
+    end_time = start_time + datetime.timedelta(minutes=1)
+    humanized_time = strings.humanize_time(start_time, end_time)
+    assert humanized_time == 'a minute left'
+
+
+def test_humanize_until_to_minute():
+    now = datetime.datetime.now()
+    start_time = now + datetime.timedelta(minutes=1)
+    end_time = start_time + datetime.timedelta(minutes=1)
+    humanized_time = strings.humanize_time(start_time, end_time)
+    assert humanized_time == '1 minutes'
+
+
+def test_humanize_until_to_more_than_a_day():
+    now = datetime.datetime.now()
+    start_time = now + datetime.timedelta(days=1) + datetime.timedelta(
+        minutes=2)
+    end_time = start_time + datetime.timedelta(minutes=2)
+    humanized_time = strings.humanize_time(start_time, end_time)
+    assert humanized_time == '1 day'
+
+
+def test_humanize_until_to_more_than_a_day_plural():
+    now = datetime.datetime.now()
+    start_time = now + datetime.timedelta(days=2) + datetime.timedelta(
+        minutes=2)
+    end_time = start_time + datetime.timedelta(minutes=2)
+    humanized_time = strings.humanize_time(start_time, end_time)
+    assert humanized_time == '2 days'
