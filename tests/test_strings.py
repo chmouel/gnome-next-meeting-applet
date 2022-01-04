109e767f96d7765d1fc563d612d3d07fbc6cd3ff
Chmouel Boudjnah <chmouel@chmouel.com>
2022-01-04 22:07:37 +0100

    Fix tests properly

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -17,15 +17,17 @@ def test_humanize_an_hour_left():
     start_time = datetime.datetime.now()
     end_time = start_time + datetime.timedelta(hours=1)
     humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == '1 hour left'
+    expected = '1 hour left'
+    assert got == expected
 
 
 def test_humanize_until_to_minute():
     now = datetime.datetime.now()
     start_time = now + datetime.timedelta(minutes=1)
     end_time = start_time + datetime.timedelta(minutes=1)
-    humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == '1 minute'
+    got = strings.humanize_time(start_time, end_time)
+    expected = '1 minute'
+    assert got == expected
 
 
 def test_humanize_until_to_hours_minutes():
@@ -34,8 +36,9 @@ def test_humanize_until_to_hours_minutes():
                         datetime.timedelta(minutes=11))
     end_time = start_time + datetime.timedelta(minutes=5)
 
-    humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == '1 hour and 11 minutes'
+    got = strings.humanize_time(start_time, end_time)
+    expected = '1 hour and 11 minutes'
+    assert got == expected
 
 
 def test_humanize_until_to_more_than_a_day():
@@ -43,8 +46,10 @@ def test_humanize_until_to_more_than_a_day():
     start_time = now + datetime.timedelta(days=1) + datetime.timedelta(
         minutes=2)
     end_time = start_time + datetime.timedelta(minutes=2)
-    humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == 'Tomorrow at ' + start_time.strftime("%H:%M")
+    got = strings.humanize_time(start_time, end_time)
+    expected = start_time.strftime("%a %d %b") + ' at ' + start_time.strftime(
+        "%H:%M")
+    assert got == expected
 
 
 def test_humanize_until_to_more_than_a_day_plural():
@@ -52,8 +57,10 @@ def test_humanize_until_to_more_than_a_day_plural():
     start_time = now + datetime.timedelta(days=2) + datetime.timedelta(
         minutes=2)
     end_time = start_time + datetime.timedelta(minutes=2)
-    humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == '2 days at ' + start_time.strftime("%H:%M")
+    got = strings.humanize_time(start_time, end_time)
+    expected = start_time.strftime("%a %d %b") + ' at ' + start_time.strftime(
+        "%H:%M")
+    assert got == expected
 
 
 def test_humanize_until_to_more_than_a_day_and_hours():
@@ -61,21 +68,26 @@ def test_humanize_until_to_more_than_a_day_and_hours():
     start_time = now + datetime.timedelta(days=1) + datetime.timedelta(
         hours=20) + datetime.timedelta(minutes=33)
     end_time = start_time + datetime.timedelta(minutes=2)
-    humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == 'Tomorrow at ' + start_time.strftime("%H:%M")
+    got = strings.humanize_time(start_time, end_time)
+    expected = start_time.strftime("%a %d %b") + ' at ' + start_time.strftime(
+        "%H:%M")
+    assert got == expected
 
 
 def test_humanize_until_to_more_than_two_days():
     now = datetime.datetime.now()
     start_time = now + datetime.timedelta(days=2)
     end_time = start_time + datetime.timedelta(minutes=2)
-    humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == '2 days at ' + start_time.strftime("%H:%M")
+    got = strings.humanize_time(start_time, end_time)
+    expected = start_time.strftime("%a %d %b") + ' at ' + start_time.strftime(
+        "%H:%M")
+    assert got == expected
 
 
 def test_humanize_until_ann_event_tommorow():
     now = dtparse.parse("23h00")
     start_time = now + datetime.timedelta(hours=2)
     end_time = start_time + datetime.timedelta(minutes=10)
-    humanized_time = strings.humanize_time(start_time, end_time)
-    assert humanized_time == 'Tomorrow at ' + start_time.strftime("%H:%M")
+    got = strings.humanize_time(start_time, end_time)
+    expected = 'Tomorrow at ' + start_time.strftime("%H:%M")
+    assert got == expected
